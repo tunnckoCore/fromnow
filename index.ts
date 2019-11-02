bc0d37ec6bc7024c8bff30b265a188fec9bf6099
Charlike Mike Reagent <ceo@tunnckocore.com>
2019-11-02 11:54:52 +0200

    hmm

--- index.ts
+++ index.ts
@@ -1,52 +1,4 @@
-var MIN = 60e3;
-var HOUR = MIN * 60;
-var DAY = HOUR * 24;
-var YEAR = DAY * 365;
-var MONTH = DAY * 30;
-
-export default function (date, opts) {
-	opts = opts || {};
-
-	var del = new Date(date).getTime() - Date.now();
-	var abs = Math.abs(del);
-
-	if (abs < MIN) return 'just now';
-
-	var periods = {
-		year: abs / YEAR,
-		month: (abs % YEAR) / MONTH,
-		day: (abs % MONTH) / DAY,
-		hour: (abs % DAY) / HOUR,
-		minute: (abs % HOUR) / MIN,
-	};
-
-	var k, val, keep=[], max=opts.max || MIN; // large number
-
-	for (k in periods) {
-		if (keep.length < max) {
-			val = Math.floor(periods[k]);
-			if (val || opts.zero) {
-				keep.push(val + ' ' + ((val == 1) ? k : (k + 's')));
-			}
-		}
-	}
-
-	k = keep.length; // reuse
-	max = ', '; // reuse
-
-	if (k > 1 && opts.and) {
-		if (k == 2) max = ' ';
-		keep[--k] = 'and ' + keep[k];
-	}
-
-	val = keep.join(max); // reuse
-
-	if (opts.suffix) {
-		val += (del < 0 ? ' ago' : ' from now');
-	}
-
-	return val;
-}
+import index from './src/index';
 
 export type DateOkay = Date | string | number;
 
@@ -57,4 +9,6 @@ export interface Options {
   max?: number,
 }
 
-export default function (date: DateOkay, opts?: Options): string;
+export default function fromnow(date: DateOkay, opts?: Options): string {
+  return index(date, opts)
+}
