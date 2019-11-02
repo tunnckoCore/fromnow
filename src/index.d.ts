3dbdd53579e19f02bf14513669b0a82545172cd9
Charlike Mike Reagent <ceo@tunnckocore.com>
2019-11-02 12:09:50 +0200

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,10 @@
-declare module 'fromnow' {
-	export type DateOkay = Date | string | number;
+export type DateOkay = Date | string | number;
 
-	export interface Options {
-		and?: boolean,
-		suffix?: boolean,
-		zero?: boolean,
-		max?: number,
-	}
-
-	export default function (date: DateOkay, opts?: Options): string;
+export interface Options {
+	and?: boolean,
+	suffix?: boolean,
+	zero?: boolean,
+	max?: number,
 }
+
+export default function (date: DateOkay, opts?: Options): string;
